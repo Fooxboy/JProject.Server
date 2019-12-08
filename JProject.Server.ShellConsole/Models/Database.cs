6e36af89491625091a5f7bbbe7fc64245e0f8ffb
Fooxboy <Fooxboy@yandex.ru>
2019-12-08 18:24:24 +0300

    Добавил консоль

---
+++ JProject.Server.ShellConsole/Models/Database.cs
@@ -0,0 +1,21 @@
+﻿using JProject.Server.Core.Interfaces;
+using JProject.Server.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JProject.Server.ShellConsole.Models
+{
+    public class Database : IDatabase
+    {
+        public User Login(string nick, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string nick, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JProject.Server.ShellConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JProject.Server.ShellConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Core.Server("localhost", 2021, );
+        }
+    }
+}
